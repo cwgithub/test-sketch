1639cc0c35cb6b37894eb593fdaef83acdc09a43
Chris Ward <cw@yellowdave.co.uk>
2023-04-18 21:37:14 +0100

    Working

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
+let angle = 0.03;
+let cover;
+
+function preload() {
+  cover = loadImage("./assets/DontShoutIt.jpg");
+}
+
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(1000, 1000, WEBGL);
 }
 
 function draw() {
-  background("pink");
+  background(237, 206, 159);
+  noStroke();
+
+  let degY = map(mouseX, 0, width, PI / 6, -PI / 6);
+  let degX = map(mouseY, 0, height, -PI / 6, PI / 6);
+  rotateY(degY);
+  rotateX(degX);
+
+  texture(cover);
+
+  box(750, 750, 100);
+
+  angle += 0.02;
 }

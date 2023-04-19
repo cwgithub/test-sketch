31b0daf73da0cc5a5e0fb10c6c669499591e5a7a
Chris Ward <cw@yellowdave.co.uk>
2023-04-19 19:44:29 +0100

    Multi layer version

--- sketch.js
+++ sketch.js
@@ -1,16 +1,22 @@
 let angle = 0.03;
-let cover;
+let coverBack;
+let coverFront;
+let clock;
 let mp3;
 let canvas;
 let playing = false;
+let scaler;
 
 function preload() {
-  cover = loadImage("./assets/DontShoutIt.jpg");
+  coverBack = loadImage("./assets/DontShoutIt.jpg");
+  coverFront = loadImage("./assets/DontShoutIt-front.jpg");
+  clock = loadImage("./assets/clock.jpg");
   mp3 = loadSound("./assets/DontShoutIt.mp3");
 }
 
 function setup() {
   canvas = createCanvas(1000, 1000, WEBGL);
+  scaler = width / 3800;
   canvas.mousePressed(canvasPressed);
 }
 
@@ -18,14 +24,29 @@ function draw() {
   background(237, 206, 159);
   noStroke();
 
-  let degY = map(mouseX, 0, width, PI / 6, -PI / 6);
-  let degX = map(mouseY, 0, height, -PI / 6, PI / 6);
+  let degY = map(mouseX, 0, width, PI / 6, -PI / 4);
+  let degX = map(mouseY, 0, height, -PI / 6, PI / 4);
   rotateY(degY);
   rotateX(degX);
 
-  texture(cover);
+  texture(coverBack);
 
-  box(750, 750, 100);
+  let zPlane = 00;
+  let zTint = 255;
+  let tintAdj = 10;
+
+  for (let layer = 0; layer <= 5; layer++) {
+    push();
+    tint(255, zTint);
+    zTint -= tintAdj;
+    tintAdj += tintAdj;
+    translate(0, 0, zPlane);
+    zPlane += 20;
+    plane(750, 750);
+    pop();
+  }
+
+  pop();
 
   angle += 0.02;
 }
@@ -33,9 +54,9 @@ function draw() {
 function canvasPressed() {
   if (!playing) {
     playing = true;
-
-    // playing a sound file on a user gesture
-    // is equivalent to `userStartAudio()`
     mp3.play();
+  } else {
+    playing = false;
+    mp3.pause();
   }
 }
